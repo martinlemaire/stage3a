3981f66feab638adcc20bc1fc4399d709fa49574
LEMAIRE Martin <mlemaire.25@gmail.com>
2014-05-27 15:01:24 +0200

    mise en place des fichiers en mieux

--- CrossFun.h
+++
@@ -1,5 +0,0 @@
-//CrossFun.h 
-//Header
-
-
-float CrossFun( float X_A, float Y_A, float X_B, float Y_B, float Y_C);
--- MaxFun.h
+++
@@ -1,1 +0,0 @@
-int MaxFun(int Taille, float** MF_X, int* MF_Min, float* Y_Min, int N_Min, float* X_Max, float* Y_Max, int* N_Max);
